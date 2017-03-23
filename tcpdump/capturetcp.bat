dded9c47c75009bba76fd7a98deb5f55096df5ef
acorver <abel.corver@gmail.com>
2017-03-23 18:17:56 -0400

    archived older deprecated sub-projects

--- tcpdump/capturetcp.bat
+++
@@ -1,4 +0,0 @@
-cd /d Z:/people/Abel/arena-automation/data/tcp/
-set datetimef=%date:~-4%-%date:~3,2%-%date:~0,2% %time:~0,2%-%time:~3,2%-%time:~6,2%
-"C:\Program Files\Wireshark\dumpcap.exe" -b filesize:1048576 -w "%datetimef%.cap" 
-pause
--- tcpdump/converttcp.bat
+++
@@ -1,3 +0,0 @@
-cd /d Z:\people\Abel\arena-automation\data\tcp
-"C:\Program Files\Wireshark\tshark.exe" -r test.cap -T fields -e ip.addr -e data > test.txt
-pause
