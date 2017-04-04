0bd77a3c14e58164d689058e0bfe9cb186e51847
acorver <abel.corver@gmail.com>
2017-04-04 14:24:14 -0400

    teensy trigger/sync update
    Now the lights flash only once, and the trigger is synchronized to the
    camera sync, allowing exact, automatic alignment of photron and mac data

--- teensy-sync/teensy-sync.ino
+++ teensy-sync/teensy-sync.ino
@@ -1,28 +1,16 @@
 
-#define PIN_SYNC_IN 17    // Teensy 2.0 pin F6 = Arduino Pin 17 (digital)
+#define PIN_TRIGGER_IN 17    // Teensy 2.0 pin F6 = Arduino Pin 17 (digital)
 #define PIN_SIGNAL_OUT 20 // Teensy 2.0 pin B6 = Arduino Pin 15 (digital)
 #define PIN_SIGNAL_OUT2 16 // this is the block signal
-
-//#define PIN_SYNC_IN 4    // Teensy 2.0 pin F6 = Arduino Pin 17 (analog)
-//#define PIN_SIGNAL_OUT 6 // Teensy 2.0 pin B6 = Arduino Pin 15 (analog)
-
 const int PIN_LEDS[] = {1, 3, 5, 7};
 
 #define NUM_LEDS 4
 
-#define COUNTER_MAX 16 // 4 LEDs, i.e. 2^4 states
+#define BLINK_MICROSECONDS 5000 // Assumes 200 FPS camera rate
+#define BLINK_NUM_FRAMES 5
 
-#define PULSE_WIDTH 384.61538461 // i.e. 1000 ms / 2600 Hz sampling frequency = ~0.384 ms = 384 usecs
-
-// Teensy 3.* only (Teensy 2.* doesn't support sufficiently high-resolution PWM)
-// #define PULSE_OUT_HIGH 255
-// #define PULSE_OUT_MIDDLE 100
-
-// Teensy 2.* only (Used to manually simulate PWM cycle)
-#define PWM_SAMPLING_FOLD_INCREASE 5
-
-// Current counter
-unsigned int nCounter;
+// Indicator for how many frames the LED's were just on
+int g_nNumFramesOn;
 
 void setup() {
 
@@ -30,76 +18,43 @@ void setup() {
   // Serial.begin(9600);
 
   // Init variables
-  nCounter = 0;
+  g_nNumFramesOn = 0;
 
   // Init pins
   for (int i = 0; i < NUM_LEDS; i++) {
     pinMode(PIN_LEDS[i], OUTPUT);
   }
-  pinMode(PIN_SYNC_IN, INPUT);
+  pinMode(PIN_TRIGGER_IN, INPUT);
   pinMode(PIN_SIGNAL_OUT, OUTPUT);
   
-  // Teensy 3.* only (Teensy 2.* doesn't support sufficiently high-resolution PWM)
-  //analogWriteFrequency(PIN_SIGNAL_OUT, 26000);
-
   // Init signals
   for (int i = 0; i < NUM_LEDS; i++) {
     digitalWrite(PIN_LEDS[i], LOW);
   }
   digitalWrite(PIN_SIGNAL_OUT, LOW);
-  
-  // Teensy 3.* only (Teensy 2.* doesn't support sufficiently high-resolution PWM)
-  //analogWrite(PIN_SIGNAL_OUT, LOW);
-  
-  // Confirm startup
-  // Serial.println("Teensy Sync Running!");
 }
 
 void loop() {
 
-  // Wait for positive edge of incoming sync signal
-  while (1) {
-    if (digitalRead(PIN_SYNC_IN)==HIGH) {
-      break;
+  // Turn on LED's for just 5 frames
+  if (digitalRead(PIN_TRIGGER_IN) == HIGH) {
+    if (g_nNumFramesOn < BLINK_NUM_FRAMES) {
+      // Turn all LED's on
+      for (int i = 0; i < NUM_LEDS; i++) {
+        digitalWrite(PIN_LEDS[i], HIGH );
+      }
+      delayMicroseconds(BLINK_MICROSECONDS); 
+      g_nNumFramesOn ++;
+      
+    } else if (g_nNumFramesOn == BLINK_NUM_FRAMES) {
+      
+      // Turn LED's off (== comparison ensures this happens only once... not crucial, but might save some clock cycles??)
+      for (int i = 0; i < NUM_LEDS; i++) {
+        digitalWrite(PIN_LEDS[i], LOW );
+      }
     }
-  }
-
-  // Increase Counter
-  nCounter += 1;
-  if (nCounter == COUNTER_MAX) {
-    nCounter = 0;  
-  }
-
-  // Turn LEDs on
-  for (int i = 0; i < NUM_LEDS; i++) {
-    digitalWrite(PIN_LEDS[i], (nCounter >> i) & 1 );
-  }
-  
-  // Determine pulse voltages
-  // Teensy 3.* only (Teensy 2.* doesn't support sufficiently high-resolution PWM)
-  //unsigned int v = PULSE_OUT_MIDDLE + ((nCounter >> 2) & 1) * (PULSE_OUT_HIGH-PULSE_OUT_MIDDLE);
-  
-  // Rate of PWM (1.0f = 100% duty cycle, 0.5f = 50%)
-  float fPWM = 1.0f - 0.5f * ((nCounter >> 2) & 1);
-  
-  for (int i = 0; i < (1+(nCounter & 3)); i++) {
-    
-    // Give PWM higher sampling frequency
-    for (int j = 0; j < PWM_SAMPLING_FOLD_INCREASE; j++) {
-      digitalWrite(PIN_SIGNAL_OUT, HIGH);
-      delayMicroseconds(PULSE_WIDTH * fPWM / PWM_SAMPLING_FOLD_INCREASE);
-      digitalWrite(PIN_SIGNAL_OUT, LOW);
-      delayMicroseconds(PULSE_WIDTH * (1.0f - fPWM) / PWM_SAMPLING_FOLD_INCREASE );
-    }
-    
-    digitalWrite(PIN_SIGNAL_OUT, LOW);
-    delayMicroseconds(PULSE_WIDTH);
-    
-    // Teensy 3.* only (Teensy 2.* doesn't support sufficiently high-resolution PWM)
-    //analogWrite(PIN_SIGNAL_OUT, v);
-    //delayMicroseconds(PULSE_WIDTH);
-    //analogWrite(PIN_SIGNAL_OUT, 0);
-    //delayMicroseconds(PULSE_WIDTH);
+  } else {
+    g_nNumFramesOn = 0;
   }
 } 
 
--- teensy-trigger/teensy-trigger.ino
+++ teensy-trigger/teensy-trigger.ino
@@ -1,14 +1,30 @@
 
 #define PIN_AMPLIFIED 4
 #define PIN_TEENSY_SIGNAL 21
+#define PIN_SYNC_SIGNAL 1
 #define SIGNAL_DURATION_MS 1000
+#define MIN_COOLDOWN_MS 500 // All triggers need to be 2000 ms apart...
+
+// variable indicating whether the system should send a TTL trigger pulse 
+// upon the next available risigin sync edge
+// NOTE: Currently, triggers received in the middle of the previous trigger 
+//       will be ignored!! Alternatively, we could wait and send multiple triggers...
+//       What is desirable?
+bool g_DoTrigger = true;
+
+// variable indicating if frame had already commenced at moment of trigger...
+// If so, wait for the next frame, to prevent misalignment
+bool g_SyncFrameCommenced = false; 
+
+unsigned long g_LastTrigger = 0;
 
 void setup() {
 
   // Init serial communication
   Serial.begin(9600);
 
-  // Init TTL output pin
+  // Init pins
+  pinMode(PIN_SYNC_SIGNAL, INPUT);
   pinMode(PIN_AMPLIFIED, OUTPUT);
   pinMode(PIN_TEENSY_SIGNAL, OUTPUT);
   digitalWrite(PIN_AMPLIFIED, LOW);
@@ -22,19 +38,48 @@ void loop() {
   while (Serial.available()) {
     switch ((char)Serial.read()) {
       case 'h': 
-        Serial.write("Teensy TTL Controller"); 
+        Serial.write("Teensy TTL Controller\n"); 
         break;
-      case 't':          
         
-        // Write TTL
-        digitalWrite(PIN_AMPLIFIED, HIGH);
-        digitalWrite(PIN_TEENSY_SIGNAL, HIGH);
-        delay(SIGNAL_DURATION_MS);
-        digitalWrite(PIN_AMPLIFIED, LOW);
-        digitalWrite(PIN_TEENSY_SIGNAL, LOW);
-        
-        Serial.write("!");
+      case 't':
+        if ( (millis() - g_LastTrigger) >= MIN_COOLDOWN_MS) {
+
+          g_DoTrigger = true;
+  
+          // If this trigger happened when we were already halfway through a frame, wait 
+          // for SYNC to go low first, and a new frame to start...
+          if (digitalRead(PIN_SYNC_SIGNAL) == HIGH) {
+            g_SyncFrameCommenced = true;
+          } else {
+            g_SyncFrameCommenced = false;
+          }
+        }
         break;
+
+    case '+':
+      digitalWrite(PIN_TEENSY_SIGNAL, HIGH);
+      delay(10000);
+      digitalWrite(PIN_TEENSY_SIGNAL, LOW);
+      break;
     }
+  }
+
+  if (g_DoTrigger && digitalRead(PIN_SYNC_SIGNAL) == HIGH && !g_SyncFrameCommenced) {
+    // Write TTL
+    digitalWrite(PIN_AMPLIFIED, HIGH);
+    digitalWrite(PIN_TEENSY_SIGNAL, HIGH);
+    delay(SIGNAL_DURATION_MS);
+    digitalWrite(PIN_AMPLIFIED, LOW);
+    digitalWrite(PIN_TEENSY_SIGNAL, LOW);
+    Serial.println("!");
+    // Trigger done
+    g_DoTrigger = false; 
+    g_LastTrigger = millis();
+  } 
+
+  // When the trigger happened halfway through a frame, we wait for the sync to go low, and then high again...
+  // So when the SYNC goes low, reset the "g_SyncFrameCommenced" variable.
+  if (g_SyncFrameCommenced && digitalRead(PIN_SYNC_SIGNAL) == LOW) {
+    g_SyncFrameCommenced = false;
   }
 } 
