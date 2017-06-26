5c840542cd3f9317fa4a5f180352c598cf77cca3
acorver <abel.corver@gmail.com>
2017-06-26 15:24:40 -0400

    updated log trial extraction for new arduino output format

--- analysis/postprocessing/extract_log_info.py
+++ analysis/postprocessing/extract_log_info.py
@@ -17,8 +17,9 @@ if __name__ == "__main__":
 # Imports for this script
 # =======================================================================================
 
-import os, sqlite3, re
+import os, sqlite3, re, json
 from datetime import datetime
+from shared import util
 
 # Helper function
 def getTimeStr(time): return datetime.fromtimestamp(time//1000).strftime('%Y-%m-%d %H:%M:%S')
@@ -66,7 +67,24 @@ def processFile(dataFile):
                 foLog.write(','.join([str(x) for x in list(line)]) + '\n')
         
         # Process flysim log (TODO: Parse new JSON format!!)
+        lookForNewTrial = False
         for line in logFlysim:
+            # Parse trial announcements
+            if 'Started new trial' in line[1]:
+                lookForNewTrial = True
+            if lookForNewTrial:
+                try:
+                    s = json.loads(line[1])
+                    g = [abs(s['TargetVelX_Segments'][0]),
+                         s['TargetPosZ1'],
+                         int(s['TimeUntilTrial'])/1000]
+                    actualTrialStart = line[0] + g[2]
+                    fo.write(','.join([str(x) for x in
+                         [actualTrialStart, getTimeStr(actualTrialStart), ] + list(g)]) + '\n')
+                    lookForNewTrial = False
+                except Exception as e:
+                    pass
+            # Deprecated trial format (kept here to parse older log files)
             m = re.match('Started new trial: speed=([0-9]*), height=([0-9]*), wait=([0-9]*)', line[1])
             if m != None:
                 g = m.groups()
